f5896e56fdc6b89fc3a59897003b1de31e2d52cf
deepak-dots <deepak.k@dotsquares.com>
2026-04-06 17:55:37 +0530

    import shopify.server and added registorwebhook

--- app/routes/auth.$.jsx
+++ app/routes/auth.$.jsx
@@ -1,8 +1,10 @@
 import { boundary } from "@shopify/shopify-app-react-router/server";
 import { authenticate } from "../shopify.server";
+import { registerWebhooks } from "../shopify.server";
 
 export const loader = async ({ request }) => {
   await authenticate.admin(request);
+  await registerWebhooks({ session });
 
   return null;
 };
